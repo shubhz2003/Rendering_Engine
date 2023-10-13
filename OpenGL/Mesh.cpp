fc308aa65d1ba63a6ac102648efa1f5d020b3eca
Shubhodeep Karmakar <karmaksh@shernet.sheridancollege.ca>
2023-10-12 22:49:57 -0400

    WoodBox with RedTint

--- OpenGL/Mesh.cpp
+++ OpenGL/Mesh.cpp
@@ -84,6 +84,7 @@ void Mesh::Create(Shader* _shader)
 void Mesh::Render(glm::mat4 _wvp)
 {
 	glUseProgram(m_shader->GetProgramID()); // Use our shader
+	m_shader->SetVec3("DiffuseColor", { 1.0f, 0.0f, 0.0f });
 
 	// 1st attribute buffer : vertices
 	glEnableVertexAttribArray(m_shader->GetAttrVertices());
