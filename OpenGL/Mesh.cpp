79659a446db66eb6744e0e1d5d2bd93e0ddceb36
Shubhodeep Karmakar <karmaksh@shernet.sheridancollege.ca>
2023-11-09 18:36:45 -0500

    AdvancedLighting Point Light

--- OpenGL/Mesh.cpp
+++ OpenGL/Mesh.cpp
@@ -135,6 +135,10 @@ void Mesh::SetShaderVariables(glm::mat4 _wvp)
 	// Configure Light
 	m_shader->SetVec3("light.position", m_lightPosition);
 	m_shader->SetVec3("light.color", m_lightColor);
+	m_shader->SetFloat("light.constant", 1.0f);
+	m_shader->SetFloat("light.linear", 0.09f);
+	m_shader->SetFloat("light.quadratic", 0.032f);
+
 	m_shader->SetVec3("light.ambientColor", { 0.1f, 0.1f, 0.1f });
 	m_shader->SetVec3("light.diffuseColor", { 1.0f, 1.0f, 1.0f });
 	m_shader->SetVec3("light.specularColor", { 3.0f, 3.0f, 3.0f });
